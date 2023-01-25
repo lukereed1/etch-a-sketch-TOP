a29c0ba8a58f20495a10d8c2a9ecbca963b17a49
Luke Reed <luke.reed@outlook.com.au>
2023-01-26 08:27:33 +1100

    Fixed bug on rainbow mode where some squares wouldn't change color, also tidied up code and added some comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Naming buttons and grid to add events
 const grid = document.querySelector("#grid");
 const gridSize = document.querySelector("#size");
 const blackButton = document.querySelector("#black");
@@ -5,21 +6,30 @@ const rainbowButton = document.querySelector("#rainbow");
 const eraserButton = document.querySelector("#eraser");
 const resetButton = document.querySelector("#reset");
 const slider = document.querySelector("#slider");
-let lastMode = "pick-color";
-let lastColor = "#000000";
+const colorPicker = document.querySelector("#color-picker");
+
+// Upon loading this creates the default 16x16 grid and start painting in black mode
+window.onload = () => {
+	createGrid(16);
+	startPainting("pick-color", "#000000");
+};
+
+// Events for each button
 rainbowButton.addEventListener("click", () => startPainting("rainbow"));
 blackButton.addEventListener("click", () => startPainting("pick-color", "black"));
 eraserButton.addEventListener("click", () => startPainting("eraser"));
 resetButton.addEventListener("click", () => resetGrid());
-slider.addEventListener("input", changeSize);
 
-const colorPicker = document.querySelector("#color-picker");
+// Takes input from slider for the grid size
+slider.addEventListener("input", () => changeSize(slider.value));
+
+// Sends a hex color code to painting function if user selects from the color wheel
 colorPicker.addEventListener("input", (e) => {
-	console.log(e);
-	const rgbCode = e.target.value;
-	startPainting("pick-color", rgbCode);
+	const userHexCode = e.target.value;
+	startPainting("pick-color", userHexCode);
 });
 
+// Uses input from slider to change the size of the grid
 function createGrid(size) {
 	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -29,24 +39,24 @@ function createGrid(size) {
 	}
 }
 
-function startPainting(mode, rgb) {
-	const gridSquares = document.querySelectorAll("#grid > div");
+// Paint mode
+function startPainting(mode, color) {
+	const gridSquares = document.querySelectorAll("#grid > div"); // Looks at all divs created from createGrid function
 	gridSquares.forEach((div) => {
+		// Applies the following code to each div created
 		div.addEventListener("mouseenter", (e) => {
 			if (mode === "pick-color") {
-				e.target.style.background = rgb;
+				e.target.style.background = color;
 			} else if (mode === "rainbow") {
-				let rainbowColor = Math.floor(Math.random() * 16777215).toString(16);
+				let rainbowColor = Math.floor(Math.random() * 16777215).toString(16); // Generates random hex color code
 				e.target.style.background = `#${rainbowColor}`;
-				lastUsed = "rainbow";
-				lastColor = rainbowColor;
 			} else {
-				e.target.style.background = "white";
+				e.target.style.background = "white"; // Eraser mode
 			}
 		});
 	});
 }
-
+// Resets the grid turning all squares to white
 function resetGrid() {
 	const gridSquares = document.querySelectorAll("#grid > div");
 	gridSquares.forEach((div) => {
@@ -54,15 +64,10 @@ function resetGrid() {
 	});
 }
 
-function changeSize() {
+// Changes the size of grid to whatever user selects on slider input
+function changeSize(newSize) {
+	createGrid(newSize);
 	resetGrid();
-	let newGridSize = slider.value;
-	gridSize.innerText = `${newGridSize} x ${newGridSize}`;
-	createGrid(newGridSize);
-	startPainting(lastMode, lastColor);
+	gridSize.innerText = `${newSize} x ${newSize}`; // Changes text below slider to match the grid size
+	startPainting("pick-color", "#000000"); // Reverts back to default paint mode
 }
-
-window.onload = () => {
-	createGrid(16);
-	startPainting("pick-color", "#000000");
-};
